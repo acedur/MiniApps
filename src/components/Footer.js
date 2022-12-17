8fe6742fb3e13983d184bcc0e32658ce64b278ae
Aleksandar Duranovski <aduranovski@gmail.com>
2022-12-17 22:53:40 +0100

    edit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,7 @@ import React from "react";
 import "./Footer.css";
 
 function Footer() {
-  return (
-    <div className="footer">
-      Aleksandar Duranovski&copy; all rights reserved 2022
-    </div>
-  );
+  return <div className="footer">Aleksandar Duranovski&copy; 2022</div>;
 }
 
 export default Footer;
